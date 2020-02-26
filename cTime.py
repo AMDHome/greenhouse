84d4c5109ff68ae9be8a67c7f85ff7a40e7a9e3b
Samuel Tseng <La6AMDHome@Gmail.com>
2020-02-26 02:46:28 -0800

    created a wrapper for the two different time modules

---
+++ cTime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from datetime import datetime as datetime
+import time
+
+def all():
+    t = datetime.now()
+    return t.strftime("%H:%M:%S"), t
+
+def now():
+    return datetime.now()
+
+def nowf():
+    return datetime.now().strftime("%H:%M:%S")
+
+def currSec():
+    return datetime.now().second
+
+def sleep(seconds):
+    time.sleep(seconds)
+
+def between(time, time_range):
+    if time_range[1] < time_range[0]:
+        return time >= time_range[0] or time <= time_range[1]
+    return time_range[0] <= time <= time_range[1]
+
+def diff(earlier_time, curr_time):
+    if curr_time.hour < earlier_time.hour:
+        return None
+    return (curr_time - earlier_time).seconds
